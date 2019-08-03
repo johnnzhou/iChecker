40eca653cd2988a2d7451cb17b1cf1a3ce6c58f6
Zhennan Zhou <zhouz46@uw.edu>
2019-08-03 16:37:44 +0800

    Initial Commit

---
+++ iChecker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iChecker
+//
+//  Created by JOHN ZZN on 8/3/19.
+//  Copyright © 2019 John Zhou. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
